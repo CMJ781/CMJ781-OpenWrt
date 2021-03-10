f81fad83a35b45d431868025f7badeebbe8a9b80
CMJ781 <32243470+CMJ781@users.noreply.github.com>
2021-03-10 20:55:29 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -7,3 +7,4 @@ CONFIG_CRYPTO_POLY1305_X86_64=y
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/custom/luci-app-turboacc/po/zh_Hans/turboacc.po
 sed -i 's/"解锁网易云灰色歌曲"/"解锁灰色歌曲"/g' package/feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
 sed -i 's/"上网时间控制"/"上网控制"/g' package/feeds/custom/luci-app-accesscontrol/po/zh_Hans/mia.po
+sed -i 's/"流量监控"/"流速监控"/g' package/feeds/custom/luci-app-wrtbwmon/po/zh_Hans/wrtbwmon.po
